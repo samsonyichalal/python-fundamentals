a0feb8aa971f8569a7a390bc8708b18841b8eadf
samson yichalal <samson.yichalal1@gmail.com>
2026-03-30 16:52:00 +0300

    Add program to creat a class and object

---
+++ oop/classes_objects.py
@@ -0,0 +1,14 @@
+# This program demonstrates how to create a class
+# and then create objects from that class.
+
+class Student:
+
+    def study(self):
+        print("The student is studying.")
+
+
+student1 = Student()
+student2 = Student()
+
+student1.study()
+student2.study()
